af33985750e499464d0d4e9fd0d7f11b7d7464b3
PatelDhaval07 <111784668+PatelDhaval07@users.noreply.github.com>
2023-07-23 11:48:01 +0530

    Add backend projects.

---
+++ TaskManager_Entity/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager_Entity
+{
+    public class DatabaseConnection
+    {
+        /// <summary>
+        /// ApplicationName
+        /// </summary>
+        /// <value></value>
+        public string ApplicationName { get; set; }
+
+        /// <summary>
+        /// ConnectionString
+        /// </summary>
+        /// <value></value>
+        public string ConnectionString { get; set; }
+    }
+}
